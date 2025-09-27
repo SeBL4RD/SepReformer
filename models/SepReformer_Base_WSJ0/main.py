86b1f2f52d84d2a77327f41677b0affc29d11e34
SeBL4RD <45276149+SeBL4RD@users.noreply.github.com>
2025-09-28 01:33:00 +0200

    refactor: restructure main processing logic and remove argparse dependency

--- models/SepReformer_Base_WSJ0/main.py
+++ models/SepReformer_Base_WSJ0/main.py
@@ -11,37 +11,45 @@ from utils.decorators import *
 log_file_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "log/system_log.log")
 logger.add(log_file_path, level="DEBUG", mode="w")
 
-@logger_wraps()
-def main(args):
-    
-    ''' Build Setting '''
-    # Call configuration file (configs.yaml)
-    yaml_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "configs.yaml")
-    yaml_dict = util_system.parse_yaml(yaml_path)
-    
-    # Run wandb and get configuration
-    config = yaml_dict["config"] # wandb login success or fail
-    
-    # Call DataLoader [train / valid / test / etc...]
-    dataloaders = get_dataloaders(args, config["dataset"], config["dataloader"])
-    
-    ''' Build Model '''
-    # Call network model
-    model = Model(**config["model"])
-
-    ''' Build Engine '''
-    # Call gpu id & device
-    gpuid = tuple(map(int, config["engine"]["gpuid"].split(',')))
-    device = torch.device(f'cuda:{gpuid[0]}')
-    
-    # Call Implement [criterion / optimizer / scheduler]
-    criterions = util_implement.CriterionFactory(config["criterion"], device).get_criterions()
-    optimizers = util_implement.OptimizerFactory(config["optimizer"], model.parameters()).get_optimizers()
-    schedulers = util_implement.SchedulerFactory(config["scheduler"], optimizers).get_schedulers()
-    
-    # Call & Run Engine
-    engine = Engine(args, config, model, dataloaders, criterions, optimizers, schedulers, gpuid, device)
-    if args.engine_mode == 'infer_sample':
-        engine._inference_sample(args.sample_file)
-    else:
-        engine.run()
+class MainProcessor:
+    @logger_wraps()
+    @staticmethod
+    def run(model_name, engine_mode, sample_file=None, out_wav_dir=None):
+        ''' Build Setting '''
+        # Call configuration file (configs.yaml)
+        yaml_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "configs.yaml")
+        yaml_dict = util_system.parse_yaml(yaml_path)
+        
+        # Run wandb and get configuration
+        config = yaml_dict["config"]
+        
+        # Create fake args object
+        class Args:
+            def __init__(self):
+                self.model = model_name
+                self.engine_mode = engine_mode
+                self.sample_file = sample_file
+                self.out_wav_dir = out_wav_dir
+        
+        args = Args()
+        
+        # Call DataLoader
+        dataloaders = get_dataloaders(args, config["dataset"], config["dataloader"])
+        
+        ''' Build Model '''
+        model = Model(**config["model"])
+        
+        ''' Build Engine '''
+        gpuid = tuple(map(int, config["engine"]["gpuid"].split(',')))
+        device = torch.device(f'cuda:{gpuid[0]}')
+        
+        criterions = util_implement.CriterionFactory(config["criterion"], device).get_criterions()
+        optimizers = util_implement.OptimizerFactory(config["optimizer"], model.parameters()).get_optimizers()
+        schedulers = util_implement.SchedulerFactory(config["scheduler"], optimizers).get_schedulers()
+        
+        # Call & Run Engine
+        engine = Engine(args, config, model, dataloaders, criterions, optimizers, schedulers, gpuid, device)
+        if engine_mode == 'infer_sample':
+            engine._inference_sample(sample_file)
+        else:
+            engine.run()
